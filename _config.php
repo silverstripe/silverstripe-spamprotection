d669b42cc96af2920cc021f99bb5ffc7334a2eef
Sabina Talipova <sabina.talipova@silverstripe.com>
2022-12-13 15:55:35 +1300

    DEP PHP Support in CMS5

--- _config.php
+++ _config.php
@@ -1,14 +1,1 @@
 <?php
-
-/**
- * Default configuration settings for the Spam Protection module.
- *
- * You should not put your own configuration in here rather use your
- * mysite/_config.php file
- *
- * @package spamprotection
- */
-
-use SilverStripe\Dev\Deprecation;
-
-Deprecation::notification_version('3.0', 'spamprotection');
